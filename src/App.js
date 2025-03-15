1ef665081a08b1e79d8549e009629df7f6036acf
Kaustubh Mahamuni <91269914+kaustubhmahamuni@users.noreply.github.com>
2025-03-15 12:07:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import TokenDisplay from './TokenDisplay';
 import ManageTokens from './ManageTokens';
-import { TokenProvider } from './TokenContext'; // Import TokenProvider
+import { TokenProvider } from './TokenContext'; 
 import './App.css';
 
 const App = () => {
-  // Auto-refresh functionality
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     // Logic to refresh data
-  //     window.location.reload(); // This will refresh the entire app
-  //   }, 000); // Set the interval to 30 seconds (30000 milliseconds)
-
-  //   return () => clearInterval(interval); // Cleanup on unmount
-  // }, []);
-
   return (
     <TokenProvider>
       <Router>
